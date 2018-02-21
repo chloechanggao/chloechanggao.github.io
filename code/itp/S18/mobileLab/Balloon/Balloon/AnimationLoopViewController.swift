e2bef2c1950f15a56d58a2a95a8ea2406460c3ea
Chloe Gao <chloe.changgao@gmail.com>
2018-02-21 14:58:25 -0500

    mobile lab hw:balloon

---
+++ code/itp/S18/mobileLab/Balloon/Balloon/AnimationLoopViewController.swift
@@ -0,0 +1,124 @@
+
+//  Description:
+//  Using the screen redraw loop to create animations.
+
+
+import UIKit
+
+class AnimationLoopViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    // UIView to animate.
+    var scaleView: UIView!
+    var scaleView2: UIView!
+    var scaleView3: UIView!
+    
+    
+    // Variable for tracking current scale.
+    var currentScale: CGFloat = 1.0;
+    
+    // Flag for tracking of scaling up or down.
+    var shouldScaleUp = false
+    
+    // Speed for scaling.
+    let scaleSpeed: CGFloat = 0.1
+    
+    // For view sizing.
+    let viewRadius: CGFloat = 50.0
+    
+    // Custom purple color.
+    let purpleColor = UIColor(red:0.59, green:0.24, blue:0.59, alpha:1.0)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Position
+        var xOffset = viewRadius * 2.85
+        var yOffset = viewRadius * 3.85;
+
+        // Create scale view and make it a circle.
+        scaleView = UIView(frame: CGRect(x: 0, y: 0, width: viewRadius * 2, height: viewRadius * 2))
+        scaleView.layer.cornerRadius = viewRadius
+        
+        
+        // Set background color.
+        scaleView.backgroundColor = purpleColor
+        
+        // Center on parent view.
+        scaleView.center = CGPoint(x: xOffset, y: yOffset)
+        
+        // Add to parent view.
+        self.view.addSubview(scaleView)
+        
+
+        // Move y offset down.
+        yOffset += viewRadius * 3.5;
+        xOffset += viewRadius * 1.85
+        
+        
+        //create second view
+        scaleView2 = UIView(frame: CGRect(x: 0, y: 0, width: viewRadius * 2, height: viewRadius * 2))
+        scaleView2.layer.cornerRadius = viewRadius
+        scaleView2.center = CGPoint(x: xOffset, y: yOffset)
+        scaleView2.backgroundColor = purpleColor
+        self.view.addSubview(scaleView2)
+        
+        yOffset += viewRadius * 3.5;
+        xOffset -= viewRadius * 1.85
+        
+        
+        
+        //create third view
+        scaleView3 = UIView(frame: CGRect(x: 0, y: 0, width: viewRadius * 2, height: viewRadius * 2))
+        scaleView3.layer.cornerRadius = viewRadius
+        scaleView3.center = CGPoint(x: xOffset, y: yOffset)
+        scaleView3.backgroundColor = purpleColor
+        self.view.addSubview(scaleView3)
+        
+        yOffset += viewRadius * 3.5;
+  
+        
+        
+        // Setup an the update loop.
+        let displayLink = CADisplayLink(target: self, selector: #selector(updateLoop))
+        displayLink.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+        
+        
+        
+    }
+    
+    @objc
+    func updateLoop() {
+        // Determine if scalingup or down based on flag.
+        let value = shouldScaleUp ? scaleSpeed : -scaleSpeed
+        
+        // Scale up or down view.
+        // Keep scale above 1.0 and 5.0
+        currentScale = max(currentScale + value, 1.0)
+        currentScale = min(currentScale, 1.25)
+        
+        
+        // Scale view.
+        scaleView.transform = CGAffineTransform(scaleX: currentScale, y: currentScale)
+        scaleView2.transform = CGAffineTransform(scaleX: currentScale, y: currentScale)
+        scaleView3.transform = CGAffineTransform(scaleX: currentScale, y: currentScale)
+    }
+    
+    
+    
+    // Set flag to true on screen press.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        shouldScaleUp = true
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    // Set flag to false on screen release.
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        shouldScaleUp = false
+    }
+    
+}
